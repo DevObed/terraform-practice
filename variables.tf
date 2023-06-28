cf9f11ea1a5a5be33db413407f79767cce9b2b14
devobed <obedasare173@gmail.com>
2023-06-27 22:26:21 -0400

    terraform

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "my_region" {
+  default = "us-east-1"
+
+}
+
+variable "profile" {
+  default = "default"
+
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+
+variable "team" {
+  default = "dev"
+
+}
+
+variable "protocol_type" {
+  default = "email"
+
+}
+
+variable "endpoint" {
+  default = "hdudieke@gmail.com"
+
+}
